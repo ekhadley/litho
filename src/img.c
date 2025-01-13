d91a06d4c281ae92b6aec90669e9543dd1c54f60
Ethan Hadley <ekhadley@gmail.com>
2025-01-13 12:03:49 -0600

    image struct and file load helper

--- src/img.c
+++ src/img.c
@@ -1,2 +1,15 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "../include/stb_image.h"
+
+typedef struct {
+    int width;
+    int height;
+    int channels;
+    unsigned char* img;
+} Image;
+
+Image load_input_image(const char* filename) {
+    int width, height, channels;
+    unsigned char *img = stbi_load(filename, &width, &height, &channels, 0);
+    return (Image){.width = width, .height = height, .channels = channels, .img = img};
+}
