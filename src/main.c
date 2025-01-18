fed46e268a8f2dd3a86df74869b455781df94349
Ethan Hadley <ekhadley@gmail.com>
2025-01-17 22:00:14 -0600

    function renames

--- src/main.c
+++ src/main.c
@@ -5,14 +5,18 @@
 
 int main(void) {
     const char *fname = "dorm.png";
-    Image img = load_input_image(fname);
+    Image img = loadInputImage(fname);
     if(img.img == NULL) {
         printf("Failed to load image: '%s'\n", fname);
         exit(1);
     }
     printf("Loaded image '%s' of shape (%d, %d, %d)\n", fname, img.height, img.width, img.channels);
     
-    Obj litho = makeLithoObj(img, defaultLithoOptions());
+    LithoOptions opts = defaultLithoOptions();
+    opts.has_frame = 1;
+    Obj litho = makeLithoObj(img, opts);
+    printf("\nCreated lithophane object with %d vertices and %d faces", litho.nverts, litho.nfaces);
 
     saveObj(litho, "litho.obj");
+    printf("\nSaved lithophane obj file.");
 }
